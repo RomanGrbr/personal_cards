203fe1f39d635e96f89aec6413720970bca0281f
romangrbr <romangrbr@gmail.com>
2023-11-30 20:46:34 +0300

    add models

--- personal_cards/cards/admin.py
+++ personal_cards/cards/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Card
+from .models import Card, AttributeType, Attribute, CardAttribute
 
 
-admin.register(Card)
+admin.site.register(Card)
+admin.site.register(AttributeType)
+admin.site.register(Attribute)
+admin.site.register(CardAttribute)
--- personal_cards/cards/models.py
+++ personal_cards/cards/models.py
@@ -4,12 +4,65 @@ from django.db import models
 MAX_LENGTH = 100
 
 
+class AttributeType(models.Model):
+    attr_type = models.CharField(
+        'Тип атрибута (forms.fields)', max_length=MAX_LENGTH
+    )
+
+    class Meta:
+        verbose_name = 'тип атрибута'
+        verbose_name_plural = 'Типы атрибутов'
+
+    def __str__(self):
+        return self.attr_type
+
+
+class Attribute(models.Model):
+    field_name = models.CharField('Имя поля', max_length=MAX_LENGTH)
+    attr_type = models.ForeignKey(
+        AttributeType, on_delete=models.PROTECT, related_name='attributes',
+        verbose_name='Тип атрибута (поля)'
+    )
+    label = models.CharField(
+        'Название поля (label)', max_length=MAX_LENGTH)
+    help_text = models.CharField(
+        'Текст подсказка (help_text)', max_length=MAX_LENGTH)
+
+    class Meta:
+        verbose_name = 'атрибут'
+        verbose_name_plural = 'Атрибуты'
+
+    def __str__(self):
+        return self.label
+
+
 class Card(models.Model):
     name = models.CharField('Имя', max_length=MAX_LENGTH)
     last_name = models.CharField('Фамилия', max_length=MAX_LENGTH)
 
     class Meta:
-        verbose_name = 'Карточка'
+        verbose_name = 'карточка'
+        verbose_name_plural = 'Карточки'
 
     def __str__(self):
         return self.name
+
+
+class CardAttribute(models.Model):
+    id_attribute = models.ForeignKey(
+        Attribute, on_delete=models.CASCADE, verbose_name='Атрибут',
+        related_name='cards'
+    )
+    id_card = models.ForeignKey(
+        Card, on_delete=models.CASCADE, verbose_name='Карточка',
+        related_name='attrs'
+    )
+    value = models.TextField('Значение')
+
+    class Meta:
+        verbose_name = 'значение атрибута'
+        verbose_name_plural = 'Значения атрибутов'
+
+    def __str__(self):
+        return '{} - {} - {}'.format(
+            self.id_attribute, self.id_card, self.value)
