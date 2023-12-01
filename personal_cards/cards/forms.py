db1e74144e3d1230757b93e083daae42e8a537a3
romangrbr <romangrbr@gmail.com>
2023-12-01 15:08:25 +0300

    ref

--- personal_cards/cards/forms.py
+++ personal_cards/cards/forms.py
@@ -3,7 +3,40 @@ from django import forms
 from .models import Card
 
 
+class AttrValueForm(forms.Form):
+    company = forms.CharField(label='Компания')
+
+
 class CardForm(forms.ModelForm):
     class Meta:
         model = Card
         fields = '__all__'
+
+        # def __init__(self, *args, **kwargs):
+        #     extra = kwargs.pop('extra')
+        #     super(CardForm, self).__init__(*args, **kwargs)
+        #     for field in extra:
+        #         print('{} - {}'.format(field.field, field.label))
+        #         try:
+        #             self.fields[field.field] = FORM_TYPES[field.atr_type.atr_type]
+        #
+        #         except KeyError:
+        #             raise ValidationError(
+        #                 '{} - {}'.format(
+        #                     'Недопустимый тип поля', field.atr_type.atr_type))
+        #         except Exception as err:
+        #             raise ValidationError(
+        #                 '{} - {}'.format(
+        #                     'Ошибка связанная с ', err))
+        #         # setattr(self.fields[field.field], 'label', field.label)
+        #         self.fields[field.field].label = field.label
+        #         self.fields[field.field].widget.attrs['is_uniq'] = str(
+        #             field.is_uniq).lower
+        #         self.fields[field.field].widget.attrs['atr_type'] = str(
+        #             field.atr_type)
+        #         if field.help_text:
+        #             setattr(
+        #                 self.fields[field.field],
+        #                 'help_text', field.help_text
+        #             )
+        #     print(self.fields)
--- personal_cards/cards/views.py
+++ personal_cards/cards/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
+from django.forms import formset_factory
 
-from .forms import CardForm
+from .forms import CardForm, AttrValueForm
 
 
 def index(request):
     template = 'index.html'
     context = {'form': CardForm}
     return render(request, template, context)
+
+def index(request):
+    extra_forms = 1
+    # form = AttrValueForm()
+    # AttrValueFormSet = formset_factory(AttrValueForm, extra=extra_forms, max_num=20)
+    AttrValueFormSet = formset_factory(AttrValueForm, extra=extra_forms, max_num=20)
+    # inline_formset = inlineformset_factory()
+    if request.method == 'POST':
+        if 'additems' in request.POST and request.POST['additems'] == 'true':
+            formset_dictionary_copy = request.POST.copy()
+            formset_dictionary_copy['form-TOTAL_FORMS'] = (
+                    int(formset_dictionary_copy['form-TOTAL_FORMS']) + extra_forms)
+            formset = AttrValueFormSet(formset_dictionary_copy)
+        else:
+            formset = AttrValueFormSet(request.POST)
+            if formset.is_valid():
+                print('Форма сохранена')
+    else:
+        formset = AttrValueFormSet(
+            initial=[{'name': 1, 'size': 'm', 'amount': 1}])
+    return render(
+        request, 'index.html', {'formset': formset})
